5c6027a4a0ee775fa6fe9cafd2d177d28d2d9630
douglascodes <douglasking215@gmail.com>
2013-06-02 02:41:28 -0400

    2to3

--- display.py
+++ display.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 rand = random.randrange
 
 class Environment():                                    #holds environment variables
-    def __init__(self): 
+    def __init__(self):
         self.scale = 256                        #basic unit of size for the game environment
         self.bottom = self.scale * 3            #sets a 4:3 aspect ration
         self.right = self.scale * 4
@@ -18,18 +18,14 @@ class Environment():                                    #holds environment varia
         self.screen = pygame.display.set_mode((self.right, self.bottom))    #Defines the game screen
         self.looptime = 30                      #A pacing timer to 30 FPS
         self.fire_delay = 1000                  #Default limiter for enemey/player shots
-        
+
         pygame.init()
-        pygame.display.flip()   
-        pygame.key.set_repeat(1, self.looptime) #allows for continuous key presses at looptime rate 
-        
+        pygame.display.flip()
+        pygame.key.set_repeat(1, self.looptime) #allows for continuous key presses at looptime rate
+
 def load_image(name, colorkey=None):            #Code from pygame tutorial for basic image loading
     fullname = os.path.join('data', name)
-    try:
-        image = pygame.image.load(fullname).convert()
-    except pygame.error, message:
-        print 'Cannot load image:', name
-        raise SystemExit, message
+    image = pygame.image.load(fullname).convert()
     if colorkey is not None:
         if colorkey is -1:
             colorkey = image.get_at((0,0))
@@ -37,8 +33,8 @@ def load_image(name, colorkey=None):            #Code from pygame tutorial for b
     return image, image.get_rect()
 
 class Scenery(pygame.sprite.Sprite):         #Generic scenary class
-    def __init__(self): 
-        pygame.sprite.Sprite.__init__(self)     
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         self.get_graphic()                      #Loads the individual graphic from class/sub
         self.image = self.graphic               #Copys that into the main image for instance
         self.w = self.rect.width                #Just a shorter way to read width from the rect
@@ -48,20 +44,20 @@ class Scenery(pygame.sprite.Sprite):         #Generic scenary class
         self.rect.left = 0                      #Sets the left side of tiling (0 is leftmost side of screen)
 
     def get_graphic(self):                      #method for retrieving the image
-        self.graphic, self.rect = load_image("brick1.png", -1) 
+        self.graphic, self.rect = load_image("brick1.png", -1)
 
     def get_tile_start(self):                   #Sets the floor tile start height
         self.start_tile_h = env.floor
-        
+
     def set_move_rate(self):
         self.move_rate = 2
-        
+
     def update(self, moving):                           #The ground will move at half the windspeed
         if moving:
                 self.rect.left -= (env.windspeed / self.move_rate)     #Although maybe this will change
         if self.rect.left < -(env.windspeed + self.w):  #If ground goes to far left
-            self.rect.left = -env.windspeed             #Set it to the left side (0) of the screen - windspeed 
-        for x in range((env.right / self.w) + 2 ):     
+            self.rect.left = -env.windspeed             #Set it to the left side (0) of the screen - windspeed
+        for x in range((env.right / self.w) + 2 ):
             env.screen.blit(self.image, (x*self.w + self.rect.left, self.start_tile_h))
         #Finds the number of tiles needed, and adds 2. To keep the smoothness
         #Then draws the tiles over the foreground bottom.
@@ -81,18 +77,18 @@ class Hill(Scenery):         #Creates hilly background
         self.start_tile_h = env.floor - self.rect.height    #sits above the road
 
 class Cloud(pygame.sprite.Sprite):                      #background clouds, A La mario bros
-    def __init__(self): 
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         cloudtype = rand(0,5)+1                           #Random number of clouds
         self.image, self.rect = load_image("cloud{}.png".format(cloudtype), -1) #picks from 5 images
         self.speed = (env.windspeed - 2) + cloudtype                        #speed = windspeed + cloudtype
-        self.rect.top = rand(-10, (env.floor - self.rect.height - 100) )    #sets vertical range 
-        self.rect.left = env.right + rand(1, env.scale*5)                               
+        self.rect.top = rand(-10, (env.floor - self.rect.height - 100) )    #sets vertical range
+        self.rect.left = env.right + rand(1, env.scale*5)
         self.next_update_time = 0
         #A couple things going on here. Picks randomly from 5 cloud types, each with its own image
         #And relative speed based on the windspeed. Starts them a random distance off to the right side.
         #To keep them from clumping up too much. It needs some work.
-                
+
     def update(self, current_time):                         #moves the cloud
         if self.next_update_time <= current_time:           #checks if it is time to update
             self.rect.left -= self.speed                    #moves it by the SPEED (based loosely on Wind)
@@ -100,7 +96,7 @@ class Cloud(pygame.sprite.Sprite):                      #background clouds, A La
         #Since it depends on two variables windspeed and the update time
         #only one needs to change to make a difference in the apparent speed. Perhaps raising this
         #will put less pressure on the CPU though. Don't know
-        
+
         if self.rect.right < 0: self.die()              #Off screen to right, kill it
 
     def die(self):
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import pygame, os, display, player, groups, missiles, targets, random, level
 from pygame.locals import *
 
-def exit_game(): 
+def exit_game():
     pygame.quit()
     os._exit(0)
-   
+
 def check_key():
     #check the keyboard input for one of the following: SPACE, ESC, UP, DOWN, LEFT, RIGHT
     #this first section increments the balloon left/right and up/down, based on the Vert/Horz states
     #if the players movement states are not == 0, then it operates.
     #Multiplying by the "State" turns the number negative or positive. Therefore fitting
-    # to the move "amount" statements under the player move methods.  
-    if player.p1.horz_state:                               
+    # to the move "amount" statements under the player move methods.
+    if player.p1.horz_state:
         player.p1.move_horz(display.env.step * player.p1.horz_state)
     if player.p1.vert_state:
         player.p1.move_vert(display.env.step * player.p1.vert_state)
-    
+
     #Keydown states either escape, fire or set the move Dirs.
     #This allows the movement simultaneously of horizontal and vertical
     for event in pygame.event.get():
-        if not hasattr(event, 'key'): continue 
+        if not hasattr(event, 'key'): continue
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE: exit_game()
             if event.key == K_UP: player.p1.vert_state = -1    #The negative modifier is applied for UP
@@ -29,15 +29,15 @@ def check_key():
             if event.key == K_SPACE and player.p1.fire_timer < pygame.time.get_ticks():
                 player.p1.fire_weapon()
                 player.p1.fire_timer = pygame.time.get_ticks() + display.env.fire_delay
-        #It would be silly to fire off unlimited water balloons. So it has a delay, set in 
+        #It would be silly to fire off unlimited water balloons. So it has a delay, set in
         #the display module. The "fire_timer" is an attribute of the player class.
-                        
+
         if event.type == KEYUP:     #Setting to ZERO makes the states FALSE.
-            if event.key == K_UP or event.key == K_DOWN: player.p1.vert_state = 0  
+            if event.key == K_UP or event.key == K_DOWN: player.p1.vert_state = 0
             if event.key == K_LEFT or event.key == K_RIGHT : player.p1.horz_state = 0
         #The release of any arrow key ceases movement of its axis.
-        
-            
+
+
 def check_hits():   #Collision detection between the ladies and balloon drops
     hits = pygame.sprite.groupcollide(groups.pshots, groups.targs, False, False) #Adds collisions to hits
     for x in iter(hits):                #Searches over the collision dictionary
@@ -61,10 +61,10 @@ def check_cloud():                      #Maintains the level of clouds
 def populate(curr_map):
     for x in range(groups.numof_clouds):       #creates clouds per numof_coulds
         groups.clouds.add(display.Cloud())     #adds em to cloud group
-    
+
     enemy_count = curr_map.length / display.env.scale
     kinds = ["None", "Oldlady", "Guard", "Goal"]
-    for x in range(enemy_count): 
+    for x in range(enemy_count):
         pos = rand(curr_map.pos, curr_map.length)
         kind = kinds[rand(len(kinds)-1)]
         curr_map.spawn_list.append([pos, kind])
@@ -74,10 +74,10 @@ def populate(curr_map):
     curr_map.next_spawn = curr_map.spawn_list.pop(0)        #
     curr_map.spawn_list.append([curr_map.length, "END"])   #Terminus to spawns
 
-   
+
 def spawn(curr_map):
     if curr_map.next_spawn[1] == "END": return
-    
+
     while curr_map.next_spawn[0] < curr_map.pos + display.env.right + display.env.scale * 2:
         if curr_map.next_spawn[1] == "Guard": groups.targs.add(targets.Guard(curr_map.next_spawn[0], curr_map.pos))
         elif curr_map.next_spawn[1] == "Oldlady": groups.targs.add(targets.Oldlady(curr_map.next_spawn[0], curr_map.pos))
@@ -85,20 +85,20 @@ def spawn(curr_map):
         try:
             curr_map.next_spawn = curr_map.spawn_list.pop(0)
         except:
-            print "Empty spawn list."
+            print("Empty spawn list.")
             return
 
 def map_end():
     exit_game()
-                                
-def game():   
+
+def game():
     clock = pygame.time.Clock()
     screen = display.env.screen
     curr_map = level.map
     populate(curr_map)
     curr_map.next_spawn = curr_map.spawn_list.pop(0)
     while player.p1.health:                #Game continues while P1 is alive
-        spawn(curr_map)           
+        spawn(curr_map)
         display.env.screen.fill(display.env.BLUE)           #fills the background with named color
         clock.tick(display.env.looptime)         #Paces the game to 30 fps
         now = pygame.time.get_ticks()       #sets 'NOW' to ... well... now
@@ -107,7 +107,7 @@ def game():
         curr_map.update()
         groups.road.update(curr_map.moving)    #Updates the group 'road', no time is needed. Based on pos
         groups.road.draw(screen)               #Draws the scenery
-        groups.players.draw(screen)            #updates the Player 
+        groups.players.draw(screen)            #updates the Player
         player.p1.show_health()                #Displays the health bar
         groups.targs.update(now, curr_map.pos, player.p1) #Makes the targs_grp move, needs player for shooting back
         groups.targs.draw(screen)          #Draws em to the screen
